e661f93b5507d308220193067862e82de51a9b22
Eshaftri <eshaftri@gmail.com>
2024-04-21 10:11:21 -0400

    fix: lint

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -1,19 +1,3 @@
-######################################################################
-# Copyright 2016, 2022 John J. Rofrano. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-######################################################################
-
 """
 Module: error_handlers
 """
@@ -30,12 +14,12 @@ def bad_request(error):
     """Handles bad requests with 400_BAD_REQUEST"""
     message = str(error)
     app.logger.warning(message)
-    return (
-        jsonify(
-            status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message
-        ),
-        status.HTTP_400_BAD_REQUEST,
+    response = jsonify(
+        status=status.HTTP_400_BAD_REQUEST, 
+        error="Bad Request", 
+        message=message
     )
+    return response, status.HTTP_400_BAD_REQUEST
 
 
 @app.errorhandler(status.HTTP_404_NOT_FOUND)
@@ -43,10 +27,12 @@ def not_found(error):
     """Handles resources not found with 404_NOT_FOUND"""
     message = str(error)
     app.logger.warning(message)
-    return (
-        jsonify(status=status.HTTP_404_NOT_FOUND, error="Not Found", message=message),
-        status.HTTP_404_NOT_FOUND,
+    response = jsonify(
+        status=status.HTTP_404_NOT_FOUND, 
+        error="Not Found", 
+        message=message
     )
+    return response, status.HTTP_404_NOT_FOUND
 
 
 @app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
@@ -54,14 +40,12 @@ def method_not_supported(error):
     """Handles unsupported HTTP methods with 405_METHOD_NOT_SUPPORTED"""
     message = str(error)
     app.logger.warning(message)
-    return (
-        jsonify(
-            status=status.HTTP_405_METHOD_NOT_ALLOWED,
-            error="Method not Allowed",
-            message=message,
-        ),
-        status.HTTP_405_METHOD_NOT_ALLOWED,
+    response = jsonify(
+        status=status.HTTP_405_METHOD_NOT_ALLOWED,
+        error="Method not Allowed",
+        message=message
     )
+    return response, status.HTTP_405_METHOD_NOT_ALLOWED
 
 
 @app.errorhandler(status.HTTP_409_CONFLICT)
@@ -69,14 +53,12 @@ def resource_conflict(error):
     """Handles resource conflicts with HTTP_409_CONFLICT"""
     message = str(error)
     app.logger.warning(message)
-    return (
-        jsonify(
-            status=status.HTTP_409_CONFLICT,
-            error="Conflict",
-            message=message,
-        ),
-        status.HTTP_409_CONFLICT,
+    response = jsonify(
+        status=status.HTTP_409_CONFLICT,
+        error="Conflict",
+        message=message
     )
+    return response, status.HTTP_409_CONFLICT
 
 
 @app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
@@ -84,14 +66,12 @@ def mediatype_not_supported(error):
     """Handles unsupported media requests with 415_UNSUPPORTED_MEDIA_TYPE"""
     message = str(error)
     app.logger.warning(message)
-    return (
-        jsonify(
-            status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-            error="Unsupported media type",
-            message=message,
-        ),
-        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+    response = jsonify(
+        status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        error="Unsupported media type",
+        message=message
     )
+    return response, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE
 
 
 @app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -99,11 +79,9 @@ def internal_server_error(error):
     """Handles unexpected server error with 500_SERVER_ERROR"""
     message = str(error)
     app.logger.error(message)
-    return (
-        jsonify(
-            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            error="Internal Server Error",
-            message=message,
-        ),
-        status.HTTP_500_INTERNAL_SERVER_ERROR,
+    response = jsonify(
+        status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        error="Internal Server Error",
+        message=message
     )
+    return response, status.HTTP_500_INTERNAL_SERVER_ERROR
